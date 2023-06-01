b100977a7a2314bfc5e9ad2e34ea0374ff8cead9
Mehir <mehir00@yahoo.se>
2023-06-01 18:34:37 +0200

    fixat 2 views, en för sökning och en för skapa items

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const app = express();
-const express_waf_middleware = require("express-waf-middleware");
+const handlebars = require('express-handlebars');
+const path = require('path');
+const hbs = handlebars.create({ defaultLayout:'main' });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-const crypto = require('crypto'); // nodejs package for generating random nonce values
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+/* const crypto = require('crypto'); // nodejs package for generating random nonce values
 let nonce = crypto.randomBytes(16).toString('base64');
 
+// start sidan ska vara att man har en text box och det man skriver in ska sparas i databas och visas på sidan
 app.use((req, res, next) => {       // middleware using csp with nonce values for script-src
     res.set({
         'Content-Security-Policy':      // set to report only during dev
         `default-src 'self'; script-src 'nonce-${nonce}'`
     });
     next();
-});
+}); */
+
+
+let listArr = [];
 
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.render('main', {name: "Johan", age: 30, list: listArr});
+});
+
+app.post('/add', (req, res) => {
+    const item = req.body.item;
+    listArr.push(item);
+    res.redirect('/');
 });
 
 app.get('/search', (req, res) => {
-    const query = req.query.q;
-    res.send(`You searched for ${query}`);
+    res.render('search');
 });
 
 app.listen(3000, () => {
