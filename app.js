0dcdf9145881a42050fe020b5e67f9072c139885
Mehir <mehir00@yahoo.se>
2023-06-02 11:24:14 +0200

    la till alla attacker

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const handlebars = require('express-handlebars');
-const path = require('path');
-const hbs = handlebars.create({ defaultLayout:'main' });
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const handlebars = require("express-handlebars");
+const path = require("path");
+const hbs = handlebars.create({ defaultLayout: "main" });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-app.set('views', path.join(__dirname, '/views'));
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 /* const crypto = require('crypto'); // nodejs package for generating random nonce values
 let nonce = crypto.randomBytes(16).toString('base64');
@@ -22,23 +22,39 @@ app.use((req, res, next) => {       // middleware using csp with nonce values fo
     next();
 }); */
 
-
 let listArr = [];
+let listResults = [];
 
-app.get('/', (req, res) => {
-    res.render('main', {name: "Johan", age: 30, list: listArr});
+app.get("/", (req, res) => {
+  res.render("main", { list: listArr });
 });
 
-app.post('/add', (req, res) => {
-    const item = req.body.item;
-    listArr.push(item);
-    res.redirect('/');
+app.post("/add", (req, res) => {
+  const item = req.body;
+  listArr.push(item);
+  res.redirect("/");
 });
 
-app.get('/search', (req, res) => {
-    res.render('search');
+app.get("/search", (req, res) => {
+  if (req.query.q) {
+    res.render("search", { results: listResults, search: req.query.q, count: listResults.length });
+  } else {
+    res.render("search");
+  }
 });
+
+app.post("/search", (req, res) => {
+    const { search, searchoption } = req.body;
+    listResults = [];
+    if (searchoption === "name") {
+      listResults = listArr.filter((item) => item.name.includes(search));
+    } else if (searchoption === "note") {
+      listResults = listArr.filter((item) => item.note.includes(search));
+    }
+  
+    res.redirect(`/search?q=${searchoption}`);
+  });
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
